e5d779c6438ad9ae6bd1376c3e7eafebb5aff1e2
marciosilvamy-creator <marciosilvamy@gmail.com>
2026-03-13 08:52:01 -0400

    Enhance error handling in get_price function
    Refactor get_price function to handle API errors and improve response validation.

--- main.py
+++ main.py
@@ -6,14 +6,29 @@ price_alert = 70000  # preço que você quer receber alerta
 
 def get_price():
     url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-    data = requests.get(url).json()
-    return float(data["price"])
+    
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        if "price" in data:
+            return float(data["price"])
+        else:
+            print("Erro na resposta da API:", data)
+            return None
+
+    except Exception as e:
+        print("Erro ao conectar com a Binance:", e)
+        return None
+
 
 while True:
     price = get_price()
-    print(f"Preço atual do {symbol}: {price}")
 
-    if price >= price_alert:
-        print("🚨 ALERTA! O preço passou do valor definido!")
+    if price:
+        print(f"Preço atual do {symbol}: {price}")
+
+        if price >= price_alert:
+            print("🚨 ALERTA! O preço passou do valor definido!")
 
     time.sleep(60)
